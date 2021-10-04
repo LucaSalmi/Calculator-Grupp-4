e90edfdfae3dc051b2087ffed9c06699fcd3c75c
Sarah <sarahlidberg@live.se>
2021-10-04 16:32:50 +0200

    forth commit

--- app/src/main/java/com/example/kalkylator/MainActivity.java
+++ app/src/main/java/com/example/kalkylator/MainActivity.java
@@ -4,22 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnPlus;
     Button btnMinus;
-    Button btn
+    Button btnDiv;
+    Button btnMulti;
+    Button btnRoten;
+    Button btnProc;
+    Button btnPyta;
+    Button btnAreaCirkel;
+    Button btnVolymCylind;
 
+    TextView head_title;
+    EditText enterNumber1;
+    EditText enterNumber2;
 
-
-
+    TextView show_symbol;
+    TextView resultat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        head_title=findViewById(R.id.head_title);
+        enterNumber1=findViewById(R.id.enterNumber1);
+        enterNumber2=findViewById(R.id.enterNumber2);
+        show_symbol=findViewById(R.id.show_symbol);
+        resultat=findViewById(R.id.resultat);
+        btnVolymCylind=findViewById(R.id.btnVolymCylind);
+        btnAreaCirkel=findViewById(R.id.btnAreaCirkel);
+        btnPyta=findViewById(R.id.btnPyta);
+        btnProc=findViewById(R.id.btnProc);
+        btnRoten=findViewById(R.id.btnRoten);
+        btnMulti=findViewById(R.id.btnMulti);
+        btnDiv=findViewById(R.id.btnDiv);
+        btnMinus=findViewById(R.id.btnMinus);
+        btnPlus=findViewById(R.id.btnPlus);
+
+        btnMinus.setOnClickListener();
+
+
     }
 }
+
+
+
 
