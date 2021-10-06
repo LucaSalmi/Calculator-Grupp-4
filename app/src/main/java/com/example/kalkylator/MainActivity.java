fc98e28a2435420f995179ab679be3e9ea06d649
LucaSalmi <luca.salmi90@gmail.com>
2021-10-06 15:08:53 +0200

    sp corr

--- app/src/main/java/com/example/kalkylator/MainActivity.java
+++ app/src/main/java/com/example/kalkylator/MainActivity.java
@@ -183,7 +183,6 @@ public class MainActivity extends AppCompatActivity {
                 showError(1);
                 return;
             }
-
             dbValue1 = Double.parseDouble(value1);
         }
 
@@ -229,7 +228,6 @@ public class MainActivity extends AppCompatActivity {
                 double sum = dbValue1 + dbValue2;
                 PrintFormat(sum);
                 break;
-
             case 2:
                 double sumMulti = dbValue1 * dbValue2;
                 PrintFormat(sumMulti);
